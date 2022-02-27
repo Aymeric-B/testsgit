ef214722902ba41c8618d4dba895c8608b03d1db
aymeric boffa <aymeric.boffa@gmail.com>
2022-02-27 15:00:19 +0100

    created uncomplete custom_operation

--- testsgit/operations.py
+++ testsgit/operations.py
@@ -1,7 +1,17 @@
 # Module for implementation of basic operations
 
+from audioop import add
+
+
 def addition(a,b,c):
   """
   Function returning addition of 3 numbers
   """
   return a+b+c
+
+def custom_operation(a,b,c):
+  """
+  Our super useful operation
+  """
+
+  return multiplication(a,b, addition(a,b,c))
